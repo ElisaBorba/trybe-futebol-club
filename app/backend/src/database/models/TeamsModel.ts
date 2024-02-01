3bc20e54e9ee73862a595fb259f1569447e6a7e1
ElisaBorba <elisaborbaa@gmail.com>
2024-02-01 04:07:06 -0300

    add Teams Migration and Model

---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,39 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+} from 'sequelize';
+import db from '.';
+
+class Teams extends Model<
+  InferAttributes<Teams>,
+  InferCreationAttributes<Teams>
+> {
+  declare id: number;
+
+  declare teamName: string;
+}
+
+Teams.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    teamName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      field: 'team_name',
+    },
+  },
+  {
+    sequelize: db,
+    modelName: 'teams',
+    timestamps: false,
+  }
+);
+
+export default Teams;
