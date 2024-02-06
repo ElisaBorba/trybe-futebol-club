7af1c9f2d78612878e3c1717a4e8b13b4b66f968
ElisaBorba <elisaborbaa@gmail.com>
2024-02-06 12:38:56 -0300

    add migrations, but missing relations

---
+++ app/backend/src/routes/matches.routes.ts
@@ -0,0 +1,11 @@
+// import { Request, Router, Response } from 'express';
+// import MatchesController from '../controllers/teams.controller';
+
+// const matchesController = new MatchesController();
+
+// const matchesRoutes = Router();
+
+// matchesRoutes.get('/', (req: Request, res: Response) =>
+// matchesController.(req, res)
+// );
+// export default matchesRoutes;
