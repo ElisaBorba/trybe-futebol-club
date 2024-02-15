687afc23550e27bfcbd5d2dc2af463b68e1852f1
ElisaBorba <elisaborbaa@gmail.com>
2024-02-15 18:43:20 -0300

    add more matches tests

--- app/backend/src/tests/Matches.test.ts
+++ app/backend/src/tests/Matches.test.ts
@@ -1,9 +1,12 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
+// @ts-ignore
 import chaiHttp = require('chai-http');
 import SequelizeMatches from '../database/models/SequelizeMatches';
 import { App } from '../app';
 import matchesMock from './mocks/Matches.mocks';
+import jwtUtils from '../utils/jwt.utils';
+import UserMocks from './mocks/User.mocks';
 
 chai.use(chaiHttp);
 
@@ -44,6 +47,41 @@ describe('Matches Endpoint Test', () => {
     expect(status).to.equal(200);
     expect(body).to.deep.equal(matchesMock.allFinishedMatches);
   });
+
+  it('Cadastra uma nova partida em andamento no banco de dados', async () => {
+    const verifyStub = sinon.stub(jwtUtils, 'verify');
+    verifyStub.returns(UserMocks.payload);
+    sinon
+      .stub(SequelizeMatches, 'create')
+      .resolves(matchesMock.matchCreated as any);
+      
+
+    const { status, body } = await chai
+      .request(app)
+      .post('/matches')
+      .set('Authorization', 'genericToken')
+      .send(matchesMock.matchToCreate);
+    expect(status).to.equal(201);
+    expect(body).to.deep.equal(matchesMock.matchCreated);
+  });
+
+  it('Não é possível cadastrar uma partida com times iguais', async () => {
+    const verifyStub = sinon.stub(jwtUtils, 'verify');
+    verifyStub.returns(UserMocks.payload);
+    sinon
+      .stub(SequelizeMatches, 'create')
+      .resolves(matchesMock.errResponseEqualTeams as any);
+      
+
+    const { status, body } = await chai
+      .request(app)
+      .post('/matches')
+      .set('Authorization', 'genericToken')
+      .send(matchesMock.equalTeams);
+    expect(status).to.equal(422);
+    expect(body).to.deep.equal(matchesMock.errResponseEqualTeams);
+  });
+
   afterEach(function () {
     sinon.restore();
   });
--- app/backend/src/tests/mocks/Matches.mocks.ts
+++ app/backend/src/tests/mocks/Matches.mocks.ts
@@ -43,8 +43,34 @@ const match3 = {
   },
 };
 
+const matchToCreate = {
+  homeTeamId: 16, 
+  awayTeamId: 8, 
+  homeTeamGoals: 2,
+  awayTeamGoals: 2
+}
+
+const matchCreated = {
+  id: 1,
+  homeTeamId: 16, 
+  awayTeamId: 8, 
+  homeTeamGoals: 2,
+  awayTeamGoals: 2,
+  inProgress: true
+}
+
+const equalTeams = {
+  id: 1,
+  homeTeamId: 16, 
+  awayTeamId: 16, 
+  homeTeamGoals: 2,
+  awayTeamGoals: 2
+}
+
+const errResponseEqualTeams = { message: "It is not possible to create a match with two equal teams"  }
+
 const allMatches = [match, match2, match3];
 const allMatchesInProgress = [match2, match3];
 const allFinishedMatches = [match];
 
-export default { allMatches, allMatchesInProgress, allFinishedMatches };
+export default { allMatches, allMatchesInProgress, allFinishedMatches, matchToCreate, errResponseEqualTeams, matchCreated, equalTeams };
